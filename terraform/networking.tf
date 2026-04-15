2f297f5ab44d364acaab5752d7de7d64f6254c32
Daniel Stuart-Kelly <daniel.stuart-kelly@cloudsphere.com>
2026-04-15 11:52:38 +0100

    extend networking to allow incoming http connections

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -64,6 +64,30 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "95.44.98.27"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "allow-frontend"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5173"
+    source_address_prefix      = "95.44.98.27"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "allow-http"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "95.44.98.27"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
